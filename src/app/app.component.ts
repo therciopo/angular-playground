e74819c5450fcc17184b6691791de5691bd1041d
thercio <therciopo@gmail.com>
2018-02-15 15:45:00 -0200

    Initial 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title1 = 'Angular App';
+  title = 'Angular App';
   name = 'Acme';
 }
