bb11b1ba07ce0eed28ad5a8e01a560b2175c050a
thercio <therciopo@gmail.com>
2018-02-15 18:36:07 -0200

    add product-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductListComponent } from './products/product-list.component';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `<div><h1>{{pageTitle}}</h1>
+  <app-products></app-products>
+  </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
