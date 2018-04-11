0984837001f4e25d8342bd683c7f7c544b8a45a0
Pires De Oliviera <tpires@indra.es>
2018-04-11 16:28:53 -0300

    Add product service

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -22,16 +22,7 @@ export class ProductDetailComponent implements OnInit {
 
      const id = +this._route.snapshot.paramMap.get('id');
      this.pageTitle += `: ${id}`;
-     this.product = {
-      'productId': id,
-      'productName': 'Leaf Rake',
-      'productCode': 'GDN-0011',
-      'releaseDate': 'March 19, 2016',
-      'description': 'Leaf rake with 48-inch wooden handle.',
-      'price': 19.95,
-      'starRating': 3,
-      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-    };
+     this.product = this.getProduct(id);
     // const param = this._route.snapshot.paramMap.get('id');
     // if (param) {
     //   const id = +param;
@@ -39,11 +30,15 @@ export class ProductDetailComponent implements OnInit {
     // }
   }
 
-  getProduct(id: number) {
-    this._productService.getProduct(id).subscribe(
-      product => this.product = product,
-      error => this.errorMessage = <any>error);
-  }
+  // getProduct(id: number) {
+  //   this._productService.getProduct(id).subscribe(
+  //     product => this.product = product,
+  //     error => this.errorMessage = <any>error);
+  // }
+
+  getProduct(id: number): IProduct {
+     return this._productService.getProduct(id);
+   }
 
   onBack(): void {
     this._router.navigate(['/products']);
