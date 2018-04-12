4e388c84a106138ae615c29d9e6718efbf370a8d
Pires De Oliviera <tpires@indra.es>
2018-04-12 14:53:26 -0300

    refactor modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from './products/product-list.component';
-import { ProductService } from './products/product.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ ProductService]
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,31 @@
 // Angular Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
 // import { HeaderComponent  } from './shared/header.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { ProductGuardService } from './products/product-guard.service';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     // HeaderComponent,
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',
-            canActivate: [ ProductGuardService ],
-       component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
-  providers: [ProductGuardService],
   bootstrap: [AppComponent/*, HeaderComponent*/]
 })
 export class AppModule { }
